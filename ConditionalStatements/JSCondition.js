d3c386ac2e54354d300425c250aa8098089fe9a8
AnuragV314 <vermagup01@gmail.com>
2023-05-25 03:59:23 +0530

    JavaScript

---
+++ ConditionalStatements/JSCondition.js
@@ -0,0 +1,14 @@
+// Conditional statements are used to perform different actions based on different conditions.
+
+greeting = "";
+time = 22;
+if (time < 10) {
+    greeting = "Good morning";
+} else if (time < 20) {
+    greeting = "Good day";
+} else {
+    greeting = "Good evening";
+}
+console.log(greeting);
+
+
