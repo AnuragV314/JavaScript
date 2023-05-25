69e54773214b0924f41fe89c12a728581ec4d8af
AnuragV314 <vermagup01@gmail.com>
2023-05-26 01:22:06 +0530

    JavaScript

---
+++ ArrowFunction/JSArrowFuncion.js
@@ -0,0 +1,9 @@
+hello = () => "Hello World!";
+console.log(hello());
+
+let myFunction = (a, b) => a * b;
+console.log(myFunction(2, 4));
+
+
+hello1 = (val) => "Hello " + val;
+console.log(hello1("Anurag"));
