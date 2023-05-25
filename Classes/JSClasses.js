d1e48f4326fc0fde709c9203459b078aae7215ba
AnuragV314 <vermagup01@gmail.com>
2023-05-26 01:29:44 +0530

    JavaScript

---
+++ Classes/JSClasses.js
@@ -0,0 +1,24 @@
+/*
+Syntax
+class ClassName {
+  constructor() { ... }
+  method_1() { ... }
+  method_2() { ... }
+  method_3() { ... }
+}
+*/
+
+class Car {
+    constructor(name, year) {
+        this.name = name;
+        this.year = year;
+    }
+    age() {
+        const date = new Date();
+        return date.getFullYear() - this.year;
+    }
+}
+
+const myCar = new Car("Ford", 2014);
+demo = "My car is " + myCar.age() + " years old.";
+console.log(demo);
