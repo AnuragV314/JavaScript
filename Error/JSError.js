2bbfde9b7fd2773f329416b4da85ef151fd09e30
AnuragV314 <vermagup01@gmail.com>
2023-05-26 00:18:52 +0530

    JavaScript

---
+++ Error/JSError.js
@@ -0,0 +1,24 @@
+/*
+Throw, and Try...Catch...Finally: 
+
+The 'try' statement defines a code block to run (to try).
+
+The 'catch' statement defines a code block to handle any error.
+
+The 'finally' statement defines a code block to run regardless of the result.
+
+The 'throw' statement defines a custom error.
+*/
+
+/*
+Syntax: 
+try {
+  Block of code to try
+}
+catch(err) {
+  Block of code to handle errors
+}
+finally {
+  Block of code to be executed regardless of the try / catch result
+}
+*/
