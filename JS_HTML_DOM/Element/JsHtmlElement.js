d3460b71b6ff9cbe7cfff6d471e16306999f3857
AnuragV314 <vermagup01@gmail.com>
2023-05-26 15:40:18 +0530

    JavaScript

---
+++ JS_HTML_DOM/Element/JsHtmlElement.js
@@ -0,0 +1,21 @@
+/*
+Finding HTML Elements:===>
+
+Finding HTML elements by id  
+Finding HTML elements by tag name
+Finding HTML elements by class name
+Finding HTML elements by CSS selectors
+Finding HTML elements by HTML object collections
+
+
+document.anchors  => document.getElementById("demo").innerHTML = "Number of anchors are: " + document.anchors.length;
+document.body
+document.documentElement
+document.embeds
+document.forms
+document.head
+document.images
+document.links
+document.scripts
+document.title
+*/
